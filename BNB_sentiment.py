1104f4ca4e2fcf08b3a7ff6c5a2a957042ccb65a
Ajay Umasankar <z5025214@unsw.edu.au>
2019-08-08 18:08:06 +1000

    HFSUAFKLFKWDIE

--- BNB_sentiment.py
+++ BNB_sentiment.py
@@ -25,16 +25,14 @@ model = clf.fit(X_train, y_train)
 stop = time.time()
 predicted_y = model.predict(X_test)
 
-
-
 # expected results vs predicted results
 # print(y_test, predicted_y)
 # print("Predict:   ", model.predict_proba(X_test))
 print("Accuracy:  ", accuracy_score(y_test, predicted_y))
-# print("Precision: ", precision_score(y_test, predicted_y, average='micro'))
-# print("Recall:    ", recall_score(y_test, predicted_y, average='micro'))
-print("Precision: ", precision_score(y_test, predicted_y, average='macro'))
-print("Recall:    ", recall_score(y_test, predicted_y, average='macro'))
+print("Precision (array): ", precision_score(y_test, predicted_y, average=None))
+# print("Precision (neg): ", precision_score(y_test, predicted_y, average=None))
+print("Precision (macro): ", precision_score(y_test, predicted_y, average='macro'))
+print("Recall (macro):    ", recall_score(y_test, predicted_y, average='macro'))
 # print("f1 micro:  ", f1_score(y_test, predicted_y, average='micro'))
 # print("f1 macro:  ", f1_score(y_test, predicted_y, average='macro'))
 print(classification_report(y_test, predicted_y))
--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,7 @@ Created on 26 Jul 2019
 import re
 import numpy as np
 import time
+import sys
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.stem import PorterStemmer 
@@ -14,30 +15,46 @@ from nltk.corpus import stopwords
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 from collections import defaultdict
 import plotly.graph_objects as go
+from nltk.stem.snowball import EnglishStemmer
 
+divider_outer = 1500
+major_sentiment = []
+major_topic = []
+instance_array = []
 sentence_array = []
 topic_array = []
 sentiment_array = []
-f = open('dataset.tsv', "r", encoding='UTF-8')
+f = open(sys.argv[1], "r", encoding='UTF-8')
+# print(sys.argv[1])
 numlines = 0
 for line in f.readlines():
     #line = line[2:]
-    numlines = numlines + 1
     line.rstrip()
     words = line.split('\t')
+#     if(words[3] != "neutral"):
+    instance_array.append(words[0])
     sentence_array.append(words[1])
     topic_array.append(words[2])
     sentiment_array.append(words[3])
+    major_sentiment.append("negative")
+    major_topic.append("10003")
+#     if(numlines == 1499): # 1500 lines processed
+#         divider_outer=len(sentence_array)
+#         print(divider_outer)
+    numlines = numlines + 1
+
+print("Sentence Array Size: ", len(sentence_array))
 
 # Filters Sentence:
     # Treats URL's as space
     # Deletes all non-alphabetic, non-[#@_$% ], non-numeric characters. 
 for i, sentence in enumerate(sentence_array):
-    sentence = re.sub("http.*?(\s|$)", " ",sentence)
+#     sentence = re.sub("http.*?(\s|$)", " ",sentence)
+    sentence = re.sub("https?://.*?(\s|$)", " ",sentence)
     sentence = re.sub("[^A-Za-z0-9#@_$% ]", "", sentence)
     sentence_array[i] = sentence
     #print (sentence)
-
+#print(len(sentence_array))
 
 
 # FreqDistribution of Sentiments/Topics
@@ -88,6 +105,22 @@ for i, sentence in enumerate(sentence_array):
     # fig.show()
 
 
+
+# VADER Analysis
+analyser = SentimentIntensityAnalyzer()
+vader_results = []
+for sentence in sentence_array[divider_outer:]:
+    score = analyser.polarity_scores(sentence)
+    compound = score.get('compound')
+    if (compound >= 0.05):
+        vader_results.append("positive")
+    elif (compound <= -0.05):
+        vader_results.append("negative")
+    else:
+        vader_results.append("neutral")
+print(vader_results)
+vader_array = np.array(vader_results)
+
 # CountVectorizer:
 # https://piazza.com/class/jvhnwcx8t2o5qg?cid=271 - can use CountVectorizer over FreqDist
     # Token pattern: Words = 2+ non-space characters 
@@ -96,73 +129,76 @@ for i, sentence in enumerate(sentence_array):
 class preprocessor (object):
     def __init__(self, divider, which, model):
         # divider = number of elements in training set
-        divider = 1500
-        ps = PorterStemmer() 
-        my_stop_words = []
-        stop_words = (stopwords.words('english')) 
-        for w in stop_words:
-            my_stop_words.append(ps.stem(w))
-
-        class StemmedCountVectorizer(CountVectorizer):
-            def build_analyzer(self):
-                analyzer = super(StemmedCountVectorizer, self).build_analyzer()
-                return lambda doc: ([ps.stem(w) for w in analyzer(doc)])
+        divider=divider_outer
+        print("divider is: ", divider)
         if(which == "topic"):
             self.y = np.array(topic_array)
         else:
             self.y = np.array(sentiment_array)
         if(model == "dt"):
             self.count = CountVectorizer(token_pattern='([^\s]{2,})', lowercase=False, max_features=200)
-        else:   # can use token_pattern='([@#$%_A-Za-z0-9]{2,})' also 
-            self.count = CountVectorizer(token_pattern='([^\s]{2,})', lowercase=False)
-            self.count = StemmedCountVectorizer(token_pattern='([^\s]{2,})', lowercase=False, analyzer="word", stop_words=stopwords.words('english'))
+        elif(model == "mine"):   # can use token_pattern='([@#$%_A-Za-z0-9]{2,})' also 
+            self.count = CountVectorizer(token_pattern='([^\s]{2,})', lowercase=True)
+        else:
+            self.count = CountVectorizer(token_pattern='([^\s]{2,})', lowercase=False) 
+            
 
-        # Stemming words in sentence, double stemming doesnt do anything
-        for sentence in sentence_array:
+#         # Stemming + Stopword removal
+#         ps = PorterStemmer() 
+#         for i, sentence in enumerate(sentence_array):
+#             print(sentence)
+#             new = ""
+#             for word in sentence.split(" "):
+#                 lower_word = word.lower()
+#                 if(lower_word not in set(stopwords.words('english'))):
+#                     stemmed = ps.stem(word)
+#                     newword = word[:len(stemmed)]
+#                     new = new + " " + newword
+#             sentence_array[i] = new.lstrip()
+#             print(new.lstrip())
+        # Stemming 
+        ps = EnglishStemmer() 
+        for i, sentence in enumerate(sentence_array):
+#             print(sentence)
             new = ""
-            for word in word_tokenize(sentence):
-                new = new + " " + ps.stem(word)
-            #print(new)
-             
-        # Creating bag of words
+            for word in sentence.split(" "):
+                stemmed = ps.stem(word)
+                newword = word[:len(stemmed)]
+                new = new + " " + newword
+            sentence_array[i] = new.lstrip()
+#             print(new.lstrip())
+           
+        # Creating bag of words  
+        # Creating X_ and Y_ train and test from bag of words
         text_data = np.array(sentence_array)
-        start = time.time()
+#         start = time.time()
         self.bag_of_words = self.count.fit_transform(text_data)
         self.X = self.bag_of_words.toarray()
-        stop = time.time()
-        print('Time: ', stop - start)  
-        #print("Divider is", divider)
+#         stop = time.time()
+#         print('Time: ', stop - start)  
         self.X_train = self.X[:divider]
         self.y_train = self.y[:divider]
         self.X_test = self.X[divider:]
         self.y_test = self.y[divider:]
         self.sentence_array = sentence_array
+        self.instance_array = instance_array
         print(self.count.get_feature_names())
         
-        # VADER Analysis
-        analyser = SentimentIntensityAnalyzer()
-        vader_results = []
-        for sentence in self.sentence_array[divider:]:
-            score = analyser.polarity_scores(sentence)
-            compound = score.get('compound')
-            if (compound >= 0.05):
-                vader_results.append("positive")
-            elif (compound <= -0.05):
-                vader_results.append("negative")
-            else:
-                vader_results.append("neutral")
-        print(classification_report(self.y_test, np.array(vader_results)))
-        print("Accuracy:  ", accuracy_score(self.y_test, np.array(vader_results)))
-        print("Precision: ", precision_score(self.y_test, np.array(vader_results), average='macro'))
-        print("Recall:    ", recall_score(self.y_test, np.array(vader_results), average='macro'))
+#         print("######################## MAJORITY CLASS ##########################")
+#         if(which == "sentiment"):
+#             majority_array = np.array(major_sentiment[divider:])
+#         else:
+#             majority_array = np.array(major_topic[divider:])
+#         print(classification_report(self.y_test, majority_array))
+#         print("Accuracy:  ", accuracy_score(self.y_test, majority_array))
+#         print("Precision (array): ", precision_score(self.y_test, majority_array, average=None))
+#         print("Precision (macro): ", precision_score(self.y_test, majority_array, average='macro'))
+#         print("Recall (macro):    ", recall_score(self.y_test, majority_array, average='macro'))
+#  
+#         print("########################## VADER ##############################")
+#         print(classification_report(self.y_test, vader_array))
+#         print("Accuracy:  ", accuracy_score(self.y_test, vader_array))
+#         print("Precision (array): ", precision_score(self.y_test, vader_array, average=None))
+#         print("Precision (macro): ", precision_score(self.y_test, vader_array, average='macro'))
+#         print("Recall (macro):    ", recall_score(self.y_test, vader_array, average='macro'))
         print("###################### Preprocessing end #########################")
-
-#         print(self.bag_of_words)
-#         visualizer = FreqDistVisualizer(features=self.count.get_feature_names())
-#         visualizer.fit(self.bag_of_words)
-#         visualizer.poof()
-
-    
-
-
-
